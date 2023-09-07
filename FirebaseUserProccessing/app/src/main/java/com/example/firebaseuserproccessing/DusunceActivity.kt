0676af6a020eefae63fbe920fcde5c5c92c4f237
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-07 17:57:27 +0300

    initial commit

--- FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/DusunceActivity.kt
+++ FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/DusunceActivity.kt
@@ -1,11 +1,29 @@
 package com.example.firebaseuserproccessing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.firebaseuserproccessing.databinding.ActivityDusunceBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class DusunceActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var binding: ActivityDusunceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dusunce)
+        binding=ActivityDusunceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth=Firebase.auth
+
+        binding.buttonCikis.setOnClickListener {
+            auth.signOut()
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
+++ FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.firebaseuserproccessing
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import com.example.firebaseuserproccessing.databinding.ActivityMainBinding
@@ -13,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
     private lateinit var auth:FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
