aa3b9dd44d9368ac16133ebf37d5b51ac9c2a8ab
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-07 17:33:57 +0300

    add login processing

---
+++ FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/DusunceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebaseuserproccessing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DusunceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dusunce)
+    }
+}
--- FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
+++ FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firebaseuserproccessing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,13 +24,25 @@ class MainActivity : AppCompatActivity() {
             auth.createUserWithEmailAndPassword(email,sifre)
                 .addOnCompleteListener(this) {task ->
                     if(task.isSuccessful){
-                        Toast.makeText(applicationContext,"kullanıcı olustu",Toast.LENGTH_LONG).show()
+                       Toast.makeText(applicationContext,"kayıt başarılı",Toast.LENGTH_LONG).show()
                     }
                 }.addOnFailureListener { exception->
                     Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
                 }
         }
-        binding.buttonGiris.setOnClickListener {  }
+        binding.buttonGiris.setOnClickListener {
+            val email=binding.emailtext.text.toString().trim()
+            val sifre=binding.parolatext.text.toString().trim()
+            auth.signInWithEmailAndPassword(email,sifre)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(applicationContext,"${auth.currentUser?.email.toString()} hoşgeldiniz",Toast.LENGTH_LONG).show()
+                        val intent=Intent(this,DusunceActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                }.addOnFailureListener{exception -> Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()}
+        }
 
     }
 
