adcac3b8206e78597e65c0fe2807af4be60e5f9e
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-07 16:57:51 +0300

    add binging main activity

--- FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
+++ FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
@@ -3,18 +3,22 @@ package com.example.firebaseuserproccessing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.firebaseuserproccessing.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth:FirebaseAuth
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         auth=Firebase.auth
+        binding.buttonKaydol.setOnClickListener {  }
+        binding.buttonGiris.setOnClickListener {  }
 
     }
-  
+
 }
