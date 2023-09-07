14f4a909898480f6baf2635c9a16ed029e01b9bb
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-07 17:15:44 +0300

    add createUserWithEmailAndPassword mainactivity

--- FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
+++ FirebaseUserProccessing/app/src/main/java/com/example/firebaseuserproccessing/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firebaseuserproccessing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.firebaseuserproccessing.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -16,7 +17,18 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth=Firebase.auth
-        binding.buttonKaydol.setOnClickListener {  }
+        binding.buttonKaydol.setOnClickListener {
+            val email=binding.emailtext.text.toString().trim()
+            val sifre=binding.parolatext.text.toString().trim()
+            auth.createUserWithEmailAndPassword(email,sifre)
+                .addOnCompleteListener(this) {task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(applicationContext,"kullanıcı olustu",Toast.LENGTH_LONG).show()
+                    }
+                }.addOnFailureListener { exception->
+                    Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+                }
+        }
         binding.buttonGiris.setOnClickListener {  }
 
     }
